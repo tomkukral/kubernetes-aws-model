5921ee88165eed5aa4551d25a4c1fac6c95ef744
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-02-06 13:37:27 +0100

    add after script

---
+++ after.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat /etc/kubernetes/addons/dns/kubedns-sa.yaml | sed 's/kube-dns/kube-dns-autoscaler/' | kubectl apply -
+
+systemctl stop kube-addon-manager
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+salt -C I@kubernetes:master service.stop kube-scheduler
+salt -C I@kubernetes:master service.stop kube-controller-manager
+systemctl start kube-scheduler
+systemctl start kube-controller-manager
