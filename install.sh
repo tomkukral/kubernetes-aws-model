bf399e0fe62f5dc1c440c875aa902e2440a62651
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-02-07 22:42:10 +0100

    add sleeps to install script

--- install.sh
+++ install.sh
@@ -16,30 +16,37 @@ salt "*" state.sls linux,openssh,salt.minion,ntp
 
 # Create and distribute SSL certificates for services using salt state
 salt '*' state.sls salt.minion.cert
+sleep 5
 
 # Install haproxy
 salt -C 'I@haproxy:proxy' state.sls haproxy
+sleep 5
 salt -C 'I@haproxy:proxy' service.status haproxy
 
 # Install docker
 salt -C 'I@docker:host' state.sls docker.host
+sleep 5
 salt -C 'I@docker:host' cmd.run "docker ps"
 
 # Install etcd
 salt -C 'I@etcd:server' state.sls etcd.server.service
+sleep 5
 salt -C 'I@etcd:server' cmd.run ". /var/lib/etcd/configenv && etcdctl cluster-health"
 
 # Install Kubernetes and Calico
 salt -C 'I@kubernetes:master' state.sls kubernetes.master.kube-addons
 salt -C 'I@kubernetes:pool' state.sls kubernetes.pool
+sleep 5
 salt -C 'I@kubernetes:pool' cmd.run "calicoctl node status"
 salt -C 'I@kubernetes:pool' cmd.run "calicoctl get ippool"
 
 # Setup NAT for Calico
 salt -C 'I@kubernetes:master' --subset 1 state.sls etcd.server.setup
+sleep 5
 
 # Run whole master to check consistency
 salt -C 'I@kubernetes:master' -b 1 state.sls kubernetes exclude=kubernetes.master.setup
+sleep 5
 
 # Register addons
 salt -C 'I@kubernetes:master' --subset 1 state.sls kubernetes.master.setup
