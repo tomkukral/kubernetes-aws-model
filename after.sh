a873b13ed4b6d7dada0bca074760733d5f73bfc9
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-06-19 15:41:11 +0200

    update after

--- after.sh
+++ after.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cat /etc/kubernetes/addons/dns/kubedns-sa.yaml | sed 's/kube-dns/kube-dns-autoscaler/' | kubectl apply -f -
-
-systemctl stop kube-addon-manager
+#cat /etc/kubernetes/addons/dns/kubedns-sa.yaml | sed 's/kube-dns/kube-dns-autoscaler/' | kubectl apply -f -
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 salt -C I@kubernetes:master service.stop kube-scheduler
 salt -C I@kubernetes:master service.stop kube-controller-manager
+salt -C I@kubernetes:master service.stop kube-addons-manager
 systemctl start kube-scheduler
 systemctl start kube-controller-manager
