63eda88137b2fecfd65be4d815ac47e3ad37329e
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-08-06 17:35:59 +0200

    adsf

--- after.sh
+++ after.sh
@@ -8,6 +8,6 @@ kubectl label nodes --all --overwrite node-role.kubernetes.io/master-
 
 salt -C I@kubernetes:master service.stop kube-scheduler
 salt -C I@kubernetes:master service.stop kube-controller-manager
-salt -C I@kubernetes:master service.stop kube-addons-manager
+salt -C I@kubernetes:master service.stop kube-addon-manager
 systemctl start kube-scheduler
 systemctl start kube-controller-manager
